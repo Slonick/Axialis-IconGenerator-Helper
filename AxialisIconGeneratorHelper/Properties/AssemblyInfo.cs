d64f602710892630295ef03a53e5c70d82c43e51
Slonick <slonick2012@gmail.com>
2019-05-05 02:36:11 +0300

    Update assembly info

--- AxialisIconGeneratorHelper/Properties/AssemblyInfo.cs
+++ AxialisIconGeneratorHelper/Properties/AssemblyInfo.cs
@@ -1,20 +1,19 @@
 ﻿#region Usings
 
 using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Windows;
 
 #endregion
 
 [assembly: AssemblyTitle("Axialis Icon Generator Helper")]
 [assembly: AssemblyDescription("Axialis Icon Generator Helper")]
-[assembly: AssemblyConfiguration("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
 [assembly: AssemblyCompany("Slonick")]
-[assembly: AssemblyProduct("Axialis Icon Generator Helper")]
-[assembly: AssemblyCopyright("Copyright © Slonick 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
-[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.None)]
+[assembly: AssemblyProduct("AxialisIconGeneratorHelper")]
+[assembly: AssemblyCopyright("Copyright © 2019 Slonick")]
+[assembly: AssemblyTrademark("Open Source, Licensed MIT")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
